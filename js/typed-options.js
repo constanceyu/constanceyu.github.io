56311562ef95038fed55ec7bb23d290b6b04d0f2
Faith Yu <19146133+faithwyu@users.noreply.github.com>
2018-08-13 22:57:59 -0700

    homepage mostly completed

--- js/typed-options.js
+++ js/typed-options.js
@@ -4,11 +4,11 @@
 
   $(function(){
     $(".element").typed({
-      strings: ["Code", "Think", "Resolve", "Create"],
-                    typeSpeed: 100,
-                     contentType: 'html',
-                     showCursor: false,
-                     loop: true,
-                     loopCount: false,
+      strings: ["Code", "Create", "Design", "Respect"],
+                    typeSpeed: 150,
+                    contentType: 'html',
+                    showCursor: false,
+                    loop: true,
+                    loopCount: false,
       });
     });
